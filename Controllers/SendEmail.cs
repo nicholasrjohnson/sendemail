b49fa69fc1b8d353b446b079d790f7d517ae83c5
Nicholas R Johnson <njohnson@nicholasrjohnson.com>
2020-10-03 20:19:09 -0400

    Initial Commit

---
+++ Controllers/SendEmail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+
+namespace emailsender.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SendEmail : ControllerBase
+    {
+        private readonly ILogger<SendEmail> _logger;
+        private readonly IConfiguration _configuration;
+        public SendEmail(ILogger<SendEmail> logger, IConfiguration configuration) 
+        {  
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Send( string email, string body )
+        {
+           int port;
+           int.TryParse(_configuration["Smtp:Port"], out port);
+           var smtpClient = new SmtpClient(_configuration["Smtp:Host"])
+           {
+                Port = port,
+                Credentials = new NetworkCredential(_configuration["Smtp:Username"],_configuration["Smtp:Password"] ), 
+                EnableSsl = true,
+            };
+    
+            smtpClient.Send( email, "njohnson@nicholasrjohnson.com", "Message from Site", body ); 
+        }
+    }
+}
---
+++ Email.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace emailsender
+{
+    public class Email 
+    {
+        public string sender { get; set; }
+        public string body { get; set; }
+    }
+}
